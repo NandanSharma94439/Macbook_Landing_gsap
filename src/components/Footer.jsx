1740e152d907a06fb90e839c3f2f4e0fb98a1661
Nandan Sharma <nandansharma94439@gmail.com>
2026-09-17 11:23:19 +0530

    Completed the project

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,26 @@
 const Footer = () => {
     return (
+        <footer>
+            <div className="info">
+                <p>
+                    More ways to shop: Find an <span>Apple store </span> or other retailers near you.Or call 000800 040 1966.
+                </p>
+                <img src="logo.svg" alt ="Apple logo"/>
+            </div>
+            <br />
 
+            <div className="links">
+                <p>©Copyright © 2024 Apple Inc. All rights reserved.</p>
+
+                <ul>
+                    {footerLinks.map(({ label, link }) => (
+                        <li key={label}>
+                            <a href={link}>{label}</a>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </footer>
     )
 }
 export default Footer
